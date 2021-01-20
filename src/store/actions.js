816358a3a04dcdf648cedd53ee09c1e82ca47427
nyqykk <1327719263@qq.com>
2021-01-20 14:44:24 +0800

    feat: 状态码更新 toast更新

--- src/store/actions.js
+++ src/store/actions.js
@@ -3,14 +3,17 @@ export async function asyncGradeList(store, userInfo){
   let res = await axios.post('http://119.29.4.93:5000',{
 		username: userInfo.username,
 		pwd: userInfo.pwd
-	}, { timeout: 8000})
+	}, { timeout: 8000 })
 	let data = {};
-	if(res.data.code === 400){
-		data.msg = res.data.msg;
-	}else{
-	  const gradeInfo = res.data.data.data;
-		data.gradeList = gradeInfo.lastList;
-		data.position = gradeInfo.position;
+  switch (res.data.code) {
+		case 200:
+			const gradeInfo = res.data.data.data;
+			data.gradeList = gradeInfo.lastList;
+			data.position = gradeInfo.position;
+			break;
+		default:
+			data.msg = res.data.msg;
+			data.code = res.data.code;
 	}
 	store.commit('getGradeList', data);
 }
